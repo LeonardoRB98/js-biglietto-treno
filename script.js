bf26f28d4c7c151332fcfa30c86d11528db16a18
Leonardo <leonardo.ranaldo.b@gmail.com>
2020-11-11 14:53:29 +0100

    primo commit definizione prime variabile

---
+++ script.js
@@ -0,0 +1,10 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere
+var name = parseInt(prompt("chilometri da percorrere"));
+console.log(name);
+// l'età del passeggero
+var age = parseInt(prompt("passenger age"));
+console.log(age);
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
+// il prezzo del biglietto è definito in base ai km (0.21 € al km);
+// va applicato uno sconto del 20% per i minorenni;
+// va applicato uno sconto del 40% per gli over 65.
